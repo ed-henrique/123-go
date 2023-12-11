9f992e629ac49c35328d152b62b71388099ea604
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-12-11 11:26:48 -0400

    :poop: add poor timer mode implementation

--- main.go
+++ main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 
-	utils "123-go/utils"
+	u "github.com/ed-henrique/123-go/utils"
 )
 
+var flagTimer int64
+
 func main() {
-	var i int
-	now := time.Now()
+	flag.Int64Var(&flagTimer, "time", 0, "Starting time to countdown from in milliseconds")
+	flag.Parse()
 
-	go func() {
-		for {
-			fmt.Printf("\r%v", utils.FormatTime(time.Since(now).Milliseconds()))
+	if flagTimer != 0 {
+		startTime := time.Now().Add(time.Millisecond * time.Duration(flagTimer))
+
+		for time.Until(startTime) > 0 {
+			fmt.Printf("\x1b[2K\r%v", u.FormatTime(time.Until(startTime).Milliseconds()))
 		}
-	}()
 
-	fmt.Scanln(&i)
-	timeElapsed := time.Since(now)
-	fmt.Println("\rTime elapsed:", timeElapsed.String())
+		fmt.Println("\x1b[2K\\rFinished!")
+	} else {
+		var i int
+		now := time.Now()
+
+		go func() {
+			for {
+				fmt.Printf("\r%v", u.FormatTime(time.Since(now).Milliseconds()))
+			}
+		}()
+
+		fmt.Scanln(&i)
+		timeElapsed := time.Since(now)
+		fmt.Println("\rTime elapsed:", u.FormatTime(timeElapsed.Milliseconds()))
+	}
 }
